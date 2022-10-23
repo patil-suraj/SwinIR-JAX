eaca27cfa79336f634b9f4ba9d0a6bdd593b9ad4
patil-suraj <surajp815@gmail.com>
2022-10-23 17:23:16 +0200

    style

--- swin_ir_jax/__init__.py
+++ swin_ir_jax/__init__.py
@@ -1,9 +1,11 @@
 from typing import List
-import PIL
+
 import numpy as np
+import PIL
 
 from .configuration_swin_ir import SwinIRConfig
 from .modeling_flax_swin_ir import FlaxSwinIR
+
 
 def prepare_inputs(images: List["PIL.Image"], window_size=8):
     """
@@ -11,19 +13,19 @@ def prepare_inputs(images: List["PIL.Image"], window_size=8):
     """
     images = [np.array(image, dtype=np.float32) for image in images]
     images = np.concatenate([image[np.newaxis, ...] for image in images], axis=0)
-    images = images / 255.
-    
+    images = images / 255.0
+
     _, h_old, w_old, _ = images.shape
     h_pad = (h_old // window_size + 1) * window_size - h_old
     w_pad = (w_old // window_size + 1) * window_size - w_old
-    images = np.concatenate([images, np.flip(images, [1])], 1)[:, :h_old + h_pad, :, :]
-    images = np.concatenate([images, np.flip(images, [2])], 2)[:, :, :w_old + w_pad, :]
+    images = np.concatenate([images, np.flip(images, [1])], 1)[:, : h_old + h_pad, :, :]
+    images = np.concatenate([images, np.flip(images, [2])], 2)[:, :, : w_old + w_pad, :]
     return images
 
 
 def numpy_to_pil(images: np.ndarray):
     """
-        Convert a numpy image or a batch of images to a PIL image.
+    Convert a numpy image or a batch of images to a PIL image.
     """
     images = np.asarray(images, dtype=np.float32)
     if images.ndim == 3:

ccfe8ccc505696a6ab8493d080a410d60a0adde9
patil-suraj <surajp815@gmail.com>
2022-10-23 17:02:00 +0200

    add some utils

--- swin_ir_jax/__init__.py
+++ swin_ir_jax/__init__.py
@@ -1,2 +1,27 @@
+from typing import List
+import PIL
+import numpy as np
+
 from .configuration_swin_ir import SwinIRConfig
 from .modeling_flax_swin_ir import FlaxSwinIR
+
+def prepare_inputs(images: List[PIL.Image], window_size=8):
+    images = [np.array(image, dtype=np.float32) for image in images]
+    images = np.concatenate([image[np.newaxis, ...] for image in images], axis=0)
+    
+    _, h_old, w_old, _ = images.shape
+    h_pad = (h_old // window_size + 1) * window_size - h_old
+    w_pad = (w_old // window_size + 1) * window_size - w_old
+    images = np.concatenate([images, np.flip(images, [1])], 1)[:, :h_old + h_pad, :, :]
+    images = np.concatenate([images, np.flip(images, [2])], 2)[:, :, :w_old + w_pad, :]
+    return images
+
+
+def numpy_to_pil(images: np.ndarray):
+    images = np.asarray(images, dtype=np.float32)
+    if images.ndim == 3:
+        images = images[None, ...]
+    images = np.clip(images, 0, 1)
+    images = (images * 255).round().astype("uint8")
+    pil_images = [PIL.Image.fromarray(image) for image in images]
+    return pil_images
